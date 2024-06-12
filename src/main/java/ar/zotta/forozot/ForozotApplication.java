5be6cc4d4206570f88f25ec322cee387df43a897
Zotta <frikilinux@gmail.com>
2024-06-11 23:10:34 -0300

    Base dirs

---
+++ src/main/java/ar/zotta/forozot/ForozotApplication.java
@@ -0,0 +1,13 @@
+package ar.zotta.forozot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForozotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForozotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/zotta/forozot/ForozotApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.zotta.forozot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForozotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
