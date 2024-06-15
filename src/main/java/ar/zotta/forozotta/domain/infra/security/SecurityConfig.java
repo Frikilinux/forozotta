37ab8f6b3e65a5e6f2b2f875ca3777f8f4a9cc22
Zotta <frikilinux@gmail.com>
2024-06-14 21:34:12 -0300

    Add security config

---
+++ src/main/java/ar/zotta/forozotta/domain/infra/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ar.zotta.forozotta.domain.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.csrf(csrf -> csrf.disable())
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return httpSecurity.build();
+  }
+
+}
