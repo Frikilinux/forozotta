af783c52578a026306940097ec18c80be58231c7
Zotta <frikilinux@gmail.com>
2024-07-06 22:00:15 -0300

    Format LocalDateTime response

--- src/main/java/ar/zotta/forozotta/domain/topic/TopicCreateResponseDto.java
+++ src/main/java/ar/zotta/forozotta/domain/topic/TopicCreateResponseDto.java
@@ -1,12 +1,14 @@
 package ar.zotta.forozotta.domain.topic;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import ar.zotta.forozotta.domain.user.UserResponseDto;
 
 public record TopicCreateResponseDto(Long id, String title, String message, UserResponseDto author,
     LocalDateTime createdAt, LocalDateTime modifiedAt) {
   public TopicCreateResponseDto(Topic topic) {
     this(topic.getId(), topic.getTitle(), topic.getMessage(), new UserResponseDto(topic.getAuthor()),
-        topic.getCreatedAt(), topic.getModifiedAt());
+        topic.getCreatedAt().truncatedTo(ChronoUnit.SECONDS), topic.getModifiedAt().truncatedTo(ChronoUnit.SECONDS));
   }
 }
--- src/main/java/ar/zotta/forozotta/infra/security/AuthResponseDto.java
+++ src/main/java/ar/zotta/forozotta/infra/security/AuthResponseDto.java
@@ -1,11 +1,13 @@
 package ar.zotta.forozotta.infra.security;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import ar.zotta.forozotta.domain.user.User;
 
-public record AuthResponseDto(Long id, String email, String access_token, String token_type, Date expires_date) {
+public record AuthResponseDto(Long id, String email, String access_token, String token_type,
+    LocalDateTime expires_date) {
   public AuthResponseDto(User user, String token, Date espiresAt) {
-    this(user.getId(), user.getEmail(), token, "Bearer", espiresAt);
+    this(user.getId(), user.getEmail(), token, "Bearer", new java.sql.Timestamp(espiresAt.getTime()).toLocalDateTime());
   }
 }
